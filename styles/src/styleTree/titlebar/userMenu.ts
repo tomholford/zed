01d9ca2412f860aa8807ca39244e3f1faf19aaea
Nate Butler <iamnbutler@gmail.com>
2023-06-09 13:56:30 -0400

    Add userMenuDropdown, dropdownDivider

--- styles/src/styleTree/titlebar/userMenu.ts
+++ styles/src/styleTree/titlebar/userMenu.ts
@@ -1,5 +1,8 @@
 import { ColorScheme } from "../../common";
-import { background } from "../components";
+import { background, border } from "../components";
+
+// This file is all wishful code, it won't compile right now
+// A bunch of these functions should be sorted to other places
 
 // WIP, needs to move to a global location
 enum CornerRadius {
@@ -24,7 +27,7 @@ export function loggedInUserButton(colorScheme: ColorScheme): Toggleable<Interac
             default: {
                 container: {
                     width: 24,
-                    background: background(lowest, "default"),
+                    background: background(lowest, "base", "default"),
                     cornerRadius: CornerRadius.Medium,
                     padding: {
                         top: 4,
@@ -38,22 +41,76 @@ export function loggedInUserButton(colorScheme: ColorScheme): Toggleable<Interac
                     height: 16,
                     corderRadius: CornerRadius.Full,
                     border: {
-                        color: isLight ? ImageOverlayBorder.LightTheme : ImageOverlayBorder.DarkTheme,
+                        color: isLight
+                            ? ImageOverlayBorder.LightTheme
+                            : ImageOverlayBorder.DarkTheme,
                         width: 1,
                         overlay: true
                     }
                 }
             },
-            hover: {
-                // TODO: Add hover state
+            hovered: {
+                // Rest of the styles should be pulled from default
+                // An interactive() utility function should be created to handle this
+                container: {
+                    background: background(lowest, "base", "hovered")
+                }
+            },
+            pressed: {
+                // Same as above
+                container: {
+                    background: background(lowest, "base", "hovered")
+                }
             }
         },
         active: {
             // TODO: Add active state
+            // A toggleable() utility function should be created to handle all the duplicate styles
         }
     }
 }
 
-export function userMenuDropdown(colorScheme: ColorScheme) {
+// Unsure if this should be ContainedFlex or ContainedStack or ContainedHStack
+export function userMenuDropdown(colorScheme: ColorScheme): ContainedFlex {
+    const { middle } = colorScheme
 
+    return {
+        name: "userMenuDropdown",
+        flex: {
+            container: {
+                background: background(middle, "base", "default"),
+                border: border(middle, "base", "default"),
+                cornerRadius: CornerRadius.Small,
+            },
+            spacing: 0
+        }
+    }
+}
+
+export function dropdownDivider(colorScheme: ColorScheme): Container {
+    const { middle } = colorScheme
+
+    const borderStyle = border(middle, "base", "default")
+
+    return {
+        name: "dropdownDivider",
+        container: {
+            // ? How do we do "width that fills the whole container"
+            // width: "100%"
+            width: "full",
+            height: 1,
+            background: borderStyle.color,
+        }
+    }
+}
+
+export function dropdownGroup(colorScheme: ColorScheme): Flex {
+    const { middle } = colorScheme
+
+    return {
+        name: "dropdownGroup",
+        flex: {
+            spacing: 1
+        }
+    }
 }
