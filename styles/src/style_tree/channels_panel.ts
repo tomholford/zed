56ef27e6c3cf0f42e829c31088fa1e2cc254ddbd
Nate Butler <iamnbutler@gmail.com>
2023-07-19 16:39:32 -0400

    Update Icons

--- styles/src/style_tree/channels_panel.ts
+++ styles/src/style_tree/channels_panel.ts
@@ -20,12 +20,12 @@ export default function channels_panel(): any {
         channel_tree: {
             channel_indent: 10,
             channel_name: text(theme.middle, "sans", "variant", { size: "md" }),
-            root_name: text(theme.middle, "sans", "variant", { size: "lg", weight: "bold" }),
+            root_name: text(theme.middle, "sans", "variant", { size: "md" }),
             channel_icon: toggleable_icon_button(theme, {
                 color: "variant",
                 active_color: "base",
-                icon: "icons/chevron_right_8.svg",
-                active_icon: "icons/chevron_down_8.svg",
+                icon: "icons/14/chevron_right.svg",
+                active_icon: "icons/14/chevron_down.svg",
                 flattened: false
             })
         }
